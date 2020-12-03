f044d60586ea47bd874b5d4937424586ccfccdee
Susan Wiens <susanmichellewiens@gmail.com>
2020-12-02 20:18:19 -0600

    wrap animal list with all the provider so it displays location and customer

--- src/components/animal/AnimalList.js
+++ src/components/animal/AnimalList.js
@@ -1,36 +1,29 @@
 import React, { useContext, useEffect } from "react"
 import { AnimalContext } from "./AnimalProvider"
+import { LocationContext } from "../location/LocationProvider"
+import { CustomerContext } from "../customer/CustomerProvider"
 import { Animal } from "./Animal"
 import "./Animal.css"
 
 export const AnimalList = () => {
-    // This state changes when `getLocations()` is invoked below
     const { animals, getAnimals } = useContext(AnimalContext)
+    const { locations, getLocations } = useContext(LocationContext)
+    const { customers, getCustomers } = useContext(CustomerContext)
 
-    /*
-        What's the effect this is reponding to? Component was
-        "mounted" to the DOM. React renders blank HTML first,
-        then gets the data, then re-renders.
-    */
     useEffect(() => {
         console.log("AnimalList: Initial render before data")
-        getAnimals()
+        getLocations()
+        .then(getCustomers)
+        .then(getAnimals)
     }, [])
 
-    /*
-        This effect is solely for learning purposes. The effect
-        it is responding to is that the location state changed.
-    */
-    useEffect(() => {
-        console.log("AnimalList: Animal state changed")
-        console.log(animals)
-    }, [animals])
-
-    return (
-        <div className="animals">
-        {
-            animals.map(animal => <Animal key={animal.id} animal={animal} />)
-        }
-        </div>
-    )
+    return animals.map(animal => {
+        const owner = customers.find(c => c.id === animal.customerId)
+        const clinic = locations.find(l => l.id === animal.locationId)
+    
+        return <Animal key={animal.id}
+                    location={clinic}
+                    customer={owner}
+                    animal={animal} />
+    })
 }
